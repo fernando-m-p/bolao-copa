95314bc7401a904c2c4b2c32c21175fc8752c250
FERNANDO MORAIS PEIXOTO <fernando.peixoto@trt22.jus.br>
2026-05-29 22:44:02 -0300

    fix: test 2

--- server.js
+++
@@ -1,8 +0,0 @@
-const { createApp } = require('./lib/app-express');
-
-const app = createApp();
-const port = process.env.PORT || 3000;
-
-app.listen(port, () => {
-    console.log(`Dev server rodando em http://localhost:${port}/`);
-});
